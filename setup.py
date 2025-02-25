c5402d01c6406dbaa45cbe2e6fb8061f9e3249fa
RomanVPX <romanvpx@icloud.com>
2025-02-25 03:45:01 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="resolution_suggester",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        line.strip()
+        for line in open("requirements.txt")
+        if line.strip() and not line.startswith("#")
+    ],
+    entry_points={
+        "console_scripts": [
+            "resolution-suggester=resolution_suggester.main:main",
+        ],
+    },
+)
