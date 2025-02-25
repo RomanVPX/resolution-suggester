a33e3623183508feb897bb907f2721ad50a74dcf
RomanVPX <romanvpx@icloud.com>
2025-02-25 03:47:03 +0300

    Renaming

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="resolution_suggester",
+    name="resolution-suggester",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
@@ -12,7 +12,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "resolution-suggester=resolution_suggester.main:main",
+            "resolution-suggester=resolution-suggester.main:main",
         ],
     },
 )
