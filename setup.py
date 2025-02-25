4d02830c003d70eb4de47808b47e6bbd385695f0
RomanVPX <romanvpx@icloud.com>
2025-02-25 05:20:49 +0300

    Dev in setup

--- setup.py
+++ setup.py
@@ -10,6 +10,13 @@ setup(
         for line in open("requirements.txt")
         if line.strip() and not line.startswith("#")
     ],
+    extras_require={
+        "dev": [
+            line.strip()
+            for line in open("requirements-dev.txt")
+            if line.strip() and not line.startswith("#")
+        ],
+    },
     entry_points={
         "console_scripts": [
             "resolution_suggester=resolution_suggester.main:main",
